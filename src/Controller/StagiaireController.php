d134a87e510e0fb99c32253a8a0ea306893bd625
Semantic-Table <89461837+Semantic-Table@users.noreply.github.com>
2022-08-16 14:28:17 +0200

    appAuthenticator registrationform et composer.json (#4)

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\MealRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
+#[Route('/stagiaire', name: 'stagiaire')]
 class StagiaireController extends AbstractController
 {
-    #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    #[Route('/', name: '_decompte')]
+    public function decompte(Request $request, MealRepository $mealRepository): Response
     {
-        return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
-        ]);
+       // $meals = $mealRepository->findBy(['card' => $this->getUser()->getUserIdentifier()]);
+
+        $calendar = [];
+        for ($i = 0; $i < 21; $i++) {
+            $calendar[$i] = strtotime('+' . $i . ' days');
+        }
+
+        dump($calendar);
+        return $this->render('stagiaire/decompte.html.twig', compact('calendar'));
     }
 }
