46bd8727b0e16660cac2989adeb0f647ec740041
Semantic-Table <89461837+Semantic-Table@users.noreply.github.com>
2022-08-17 09:42:34 +0200

    Mlp stagiare controller (#5)
    * appAuthenticator registrationform et composer.json
    
    * :sparkles: controller stagiaire et modif formulaire registration
    
    Co-authored-by: Caliendo Julien <34681928+caliendojulien@users.noreply.github.com>

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,9 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('card')
+            ->add('name')
+            ->add('firstname')
+            ->add('grade')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
