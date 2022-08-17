b9dabb7d5d0048b58e41c12b42949e822bc790bb
Semantic-Table <89461837+Semantic-Table@users.noreply.github.com>
2022-08-17 14:57:46 +0200

    Mlp section head controller (#8)
    * appAuthenticator registrationform et composer.json
    
    * :sparkles: controller stagiaire et modif formulaire registration
    
    Co-authored-by: Caliendo Julien <34681928+caliendojulien@users.noreply.github.com>

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -45,7 +45,6 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
          return new RedirectResponse($this->urlGenerator->generate('main_home'));
     }
 
