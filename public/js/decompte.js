19496563cb137485421b9baf2a7689c428daf1e8
Semantic-Table <89461837+Semantic-Table@users.noreply.github.com>
2022-08-17 15:11:24 +0200

    Test (#9)
    * appAuthenticator registrationform et composer.json
    
    * :sparkles: controller stagiaire et modif formulaire registration
    
    * :wrench: controlleur sectionhead + petite reparations
    
    * test duplication

--- public/js/decompte.js
+++ public/js/decompte.js
@@ -1,3 +1,24 @@
+
+function addWeekMidi(userId,value) {
+    let date = document.getElementById('datePicker').value
+    let xhr = new XMLHttpRequest();
+    let data = "userId=" + userId + '&date=' + date + '&value=' + value.checked;
+    console.log(data)
+    xhr.open('POST', '/ChefDeSection/addWeekMidi')
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xhr.send(data);
+}
+
+function addWeekSoir(userId,value) {
+    let date = document.getElementById('datePicker').value
+    let xhr = new XMLHttpRequest();
+    let data = "userId=" + userId + '&date=' + date + '&value=' + value.checked;
+    console.log(data)
+    xhr.open('POST', '/ChefDeSection/addWeekSoir')
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xhr.send(data);
+}
+
 function updateMidi(date, userId, value) {
     let xhr = new XMLHttpRequest();
     let data = "userId=" + userId + '&date=' + date + '&value=' + value.checked;
@@ -15,3 +36,8 @@ function updateSoir(date, userId, value) {
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
     xhr.send(data);
 }
+
+function coucou() {
+    console.log('hey')
+}
+
